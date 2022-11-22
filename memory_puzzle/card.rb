b95c1144bded79870dd69f33382996f84a123ddb
Logan Hartman <loganchart@gmail.com>
2022-11-22 14:11:15 -0500

    completed card init and a few methods

--- memory_puzzle/card.rb
+++ memory_puzzle/card.rb
@@ -1,3 +1,26 @@
 class Card
 
+    def initialize(face_value)
+        @face_value = face_value
+        @visible = false
+    end
+
+    def hide
+        @visible = false
+    end
+
+    def reveal
+        @visible = true
+    end
+
+    def to_s
+        @face_value.to_s    #TODO check to_s
+    end
+
+    def ==(fv)
+        @face_value == fv   #TODO consider visible comparison
+    end
+
+
+
 end
